adbd1021ce655a4710e9cb257f9bcad12c9cd3ec
Yeison <yha.big@gmail.com>
2022-12-08 16:14:55 -0500

    Se utiliza LocalStorage para guardar el historial

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -15,7 +15,9 @@ export class GifsService {
     return [...this._historial];
   }
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+  }
 
   buscarGifs(query: string) {
     query = query.trim().toLocaleLowerCase();
@@ -23,6 +25,8 @@ export class GifsService {
     if (!this._historial.includes(query)) {
       this._historial.unshift(query);
       this._historial = this._historial.slice(0, 10);
+
+      localStorage.setItem('historial', JSON.stringify(this._historial));
     }
 
     this.http
